ae05b411e9adce5f0f7725929e0700657e8ec4bf
Laura Boetcher <g33klaura@gmail.com>
2017-10-14 20:02:54 -0400

    Initial game file

---
+++ app.js
@@ -0,0 +1,42 @@
+// Hangman Cli App
+/*
+Steps to complete:
+[] Use inquirer package
+[] Constructors for "Word" and "Letter"
+[] Keep track of user's remaining guesses
+[] Prompt user if they want to end the game if out of guesses (and replay?)
+[] Each constructor function in it's own file (use module.exports)
+[] Use function prototypes
+    https://www.thecodeship.com/web-development/methods-within-constructor-vs-prototype-in-javascript/)
+[] Push to Github - include package.json & .gitignore
+[]
+*/
+
+// Which looks better? :)
+console.log('|-------------|');
+console.log('+-------------+');
+console.log('=-------------=');
+
+// REQUIREMENTS ==========================
+//
+var inquirer = require('inquirer');
+var word = require('./word.js');
+var letter = require('./letter.js');
+
+
+
+
+// VARIABLES ==========================
+//
+
+
+
+
+// FUNCTIONS ==========================
+//
+
+
+
+
+// MAIN PROCESS ==========================
+//
