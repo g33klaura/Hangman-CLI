4fcf9013b4a438ef39f9f6ec58f2191fc027ba50
Laura Boetcher <g33klaura@gmail.com>
2017-10-18 20:33:23 -0400

    Build Letters constructor

---
+++ assets/letter.js
@@ -0,0 +1,10 @@
+// JavaScript for letter inputs of Hangman Cli App
+
+// CONSTRUCTORS ====================
+// 
+
+let Letters = function(letter) {
+	this.letter = letter;
+};
+
+module.exports = Letters;
