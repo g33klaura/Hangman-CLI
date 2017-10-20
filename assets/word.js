1e050ad4649ac70bfe9f47144fb7254997662e2a
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 16:30:25 -0400

    Need to start over; simplify game words

--- assets/word.js
+++ assets/word.js
@@ -2,7 +2,7 @@
 
 // REQUIREMENTS ====================
 // 
-const readline = require('readline');
+// const readline = require('readline');
 const fs = require('fs');
 let Letter = require('letter.js');
 
@@ -11,7 +11,12 @@ let Letter = require('letter.js');
 // VARIABLES ====================
 // 
 
+let currentWord = '';
+
+let duckNamesArray = ['darkwing', 'daisy', 'daffy', 'howard', 'donald', 'scrooge'];
+
 // Attempting to read each game word from file, line-by-line
+/*
 const rl = readline.createInterface({ 
 	input: fs.createReadStream('./game-words.txt')
 });
@@ -20,11 +25,12 @@ rl.on('line', function (line) {
 	rl.pause('\n');
 	console.log('Line from file:', line);	
 });
+*/
 // If so, how to read one line at a time..........
 // Does each line get put into an array, call the first item into new variable like 'currentWord'
 // loop through remaining words in array when game is replayed?
 
-let currentWord = '';
+
 
 
 
