df7875a75918160c295ccd862f838e45e7424bc3
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 16:37:57 -0400

    Refactor inquirer.prompt into function

--- assets/letter.js
+++ assets/letter.js
@@ -12,25 +12,6 @@ const inquirer = require('inquirer');
 
 let lettersGuessed = [];
 
-inquirer.prompt([
-	{
-		type: 'input',
-		message: 'Guess a letter',
-		name: 'letterGuess'
-	}	
-])
-.then(function(response) {
-
-	// For now, log the selected letter
-	// console.log(response.letterGuess);
-
-	// Need to store letters guessed in array?
-	lettersGuessed.push(response.letterGuess);
-	console.log(lettersGuessed);
-
-})
-
-
 
 
 // CONSTRUCTORS ====================
@@ -40,7 +21,36 @@ inquirer.prompt([
 let Letters = function(letter) {
 	this.letter = letter;
 
-
+	// Probably missing things still....
 };
 
 module.exports = Letters;
+
+
+
+// FUNCTIONS ==========================
+//
+
+function playerGuess() {
+
+	inquirer.prompt([
+		{
+			type: 'input',
+			message: 'Guess a letter',
+			name: 'letterGuess'
+		}	
+	])
+	.then(function(response) {
+
+		// For now, log the selected letter
+		// console.log(response.letterGuess);
+
+		// Need to store letters guessed in array?
+		lettersGuessed.push(response.letterGuess);
+			console.log(lettersGuessed);
+
+		// When does this need to be called? Should it be part of Letters constructor?
+		// in app.js ~> if unsolved && guesses remain, playerGuess()
+	});
+}
+
