2e2770354a9d9847e5357aff3b9c8e4de3afb2ee
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 16:30:48 -0400

    Focus on inquirer prompts

--- assets/letter.js
+++ assets/letter.js
@@ -1,10 +1,46 @@
 // JavaScript for letter inputs of Hangman Cli App
 
+// REQUIREMENTS ====================
+// 
+
+const inquirer = require('inquirer');
+
+
+
+// VARIABLES ====================
+// 
+
+let lettersGuessed = [];
+
+inquirer.prompt([
+	{
+		type: 'input',
+		message: 'Guess a letter',
+		name: 'letterGuess'
+	}	
+])
+.then(function(response) {
+
+	// For now, log the selected letter
+	// console.log(response.letterGuess);
+
+	// Need to store letters guessed in array?
+	lettersGuessed.push(response.letterGuess);
+	console.log(lettersGuessed);
+
+})
+
+
+
+
 // CONSTRUCTORS ====================
 // 
 
+// Should this be LettersGuessed ? 
 let Letters = function(letter) {
 	this.letter = letter;
+
+
 };
 
 module.exports = Letters;
